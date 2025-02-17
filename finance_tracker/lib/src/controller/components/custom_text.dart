739f5ef73dac5a2879b58cb0b323878e326a15a3
Arslan Qazi <arslanqazi1999@gmail.com>
2025-02-17 14:24:11 +0500

    Update custom_text.dart

--- finance_tracker/lib/src/controller/components/custom_text.dart
+++ finance_tracker/lib/src/controller/components/custom_text.dart
@@ -24,3 +24,29 @@ class CustomText extends StatelessWidget {
     );
   }
 }
+
+class BoldText extends StatelessWidget {
+  String text;
+  Color color;
+  double fontSize;
+  FontWeight fontWeight;
+  BoldText({super.key,
+    required this.text,
+    this.color=Colors.black,
+    this.fontWeight=FontWeight.bold,
+    this.fontSize=18,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(
+        color: color,
+        fontSize: fontSize,
+        fontWeight: fontWeight,
+      ),
+    );
+  }
+}
+
