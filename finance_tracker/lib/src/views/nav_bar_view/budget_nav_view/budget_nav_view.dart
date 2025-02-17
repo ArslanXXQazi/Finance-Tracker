a7ce66748bd9d40582c13bcb1fe3552164e5ae54
Arslan Qazi <arslanqazi1999@gmail.com>
2025-02-17 22:02:04 +0500

    update

--- finance_tracker/lib/src/views/nav_bar_view/budget_nav_view/budget_nav_view.dart
+++ finance_tracker/lib/src/views/nav_bar_view/budget_nav_view/budget_nav_view.dart
@@ -86,6 +86,33 @@ class BudgetNavView extends StatelessWidget {
                  padding:  EdgeInsets.symmetric(vertical: height*.01),
                  child: BoldText(text: 'No Budgeted this month'),
                ),
+               Divider(),
+               Row(
+                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                 children: [
+                Row(children: [
+                  Container(
+                    height: height*.04,
+                    width: width*.1,
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade100,
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(color: Colors.black,width: 2),
+                    ),
+                    child: Icon(Icons.shopping_cart_outlined),
+                  ),
+                  SizedBox(width: width*.03,),
+                  CustomText(text: 'Entertainment'),
+                ],),
+                OutlinedButton(
+                    onPressed: (){},
+                    style: OutlinedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      )
+                    ),
+                    child: CustomText(text: 'SET BUDGET',fontWeight: FontWeight.bold,))
+               ],)
              ],),
           )
 
