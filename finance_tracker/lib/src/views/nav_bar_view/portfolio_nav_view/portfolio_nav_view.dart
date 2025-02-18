d7a6f53b8e213e0dcd605c8224bd433887e8704c
Arslan Qazi <arslanqazi1999@gmail.com>
2025-02-18 20:51:49 +0500

    Update portfolio_nav_view.dart

--- finance_tracker/lib/src/views/nav_bar_view/portfolio_nav_view/portfolio_nav_view.dart
+++ finance_tracker/lib/src/views/nav_bar_view/portfolio_nav_view/portfolio_nav_view.dart
@@ -1,13 +1,36 @@
-import 'package:finance_tracker/src/controller/components/custom_text.dart';
-import 'package:flutter/material.dart';
+
+
+import 'package:finance_tracker/src/controller/constant/linker.dart';
 
 class PortfolioNavView extends StatelessWidget {
   const PortfolioNavView({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final height=MediaQuery.sizeOf(context).height;
+    final width=MediaQuery.sizeOf(context).width;
     return Scaffold(
-      body: Center(child: BoldText(text: 'PortfolioNavView'),),
+      backgroundColor: Colors.grey.shade100,
+      appBar: AppBar(
+        backgroundColor: Colors.grey.shade100,
+        title: CustomText(text: 'Portfolio',fontSize: 22,),
+        actions: [
+          InkWell(
+              onTap: (){},
+              child: ImageIcon(AssetImage(Appimages.search),size: width*.09,)),
+          SizedBox(width: width*.05,),
+          InkWell(
+              onTap: (){},
+              child: ImageIcon(AssetImage(Appimages.scanner),size: width*.075)),
+          SizedBox(width: width*.04,),
+          InkWell(
+              onTap: (){},
+              child: ImageIcon(AssetImage(Appimages.more),size: width*.09)),
+        ],
+      ),
+      body: Column(children: [
+        
+      ],)
     );
   }
 }
