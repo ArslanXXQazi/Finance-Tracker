527e6aef943601411885b5a4c2eaee086ec8f5b0
Arslan Qazi <arslanqazi1999@gmail.com>
2025-02-19 14:58:42 +0500

    update

---
+++ finance_tracker/lib/src/views/nav_bar_view/transcation_nav_view/settle_up_view.dart
@@ -0,0 +1,14 @@
+import 'package:finance_tracker/src/controller/constant/linker.dart';
+
+class SettleUpView extends StatelessWidget {
+  const SettleUpView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(children: [
+
+      ],),
+    );
+  }
+}
